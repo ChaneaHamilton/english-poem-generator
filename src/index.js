c09c75d77bb0942a71d3c38772373f501748b5bf
ChaneaHamilton <chaneahamilton@gmail.com>
2025-03-27 10:33:01 -0600

    added basic js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: ["Generating poem, please wait..."],
+    autoStart: true,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
